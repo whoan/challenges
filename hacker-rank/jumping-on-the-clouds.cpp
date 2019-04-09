6c0d8c527f40f62a1896851b805c9784a0b60f41
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-04-09 14:33:17 -0300

    Add jumping-on-the-clouds challenge

---
+++ hacker-rank/jumping-on-the-clouds.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+int jumpingOnClouds(std::vector<int> clouds) {
+  int jumps = 0;
+  for (std::size_t currentCloud(0); currentCloud < clouds.size()-1; ++currentCloud, ++jumps) {
+    if (currentCloud + 2 < clouds.size() && clouds.at(currentCloud+2) == 0) {
+      ++currentCloud;
+    }
+  }
+  return jumps;
+}
+
+/*************************************************/
+/* HERE STARTS THE PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = jumpingOnClouds(c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
