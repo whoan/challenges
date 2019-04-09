1d5ffff1f74c3576352b08c1d8e3d91cbc999c19
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-04-09 11:36:13 -0300

    Add counting-valleys challenge

---
+++ hacker-rank/counting-valleys.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+int countingValleys(int, std::string steps) {
+  int distanceFrmoSeaLevel = 0;
+  int valleysCount = 0;
+  for (auto step : steps) {
+    bool atSeaLevel = distanceFrmoSeaLevel == 0;
+    distanceFrmoSeaLevel += step == 'U' ? 1 : -1;
+    if (atSeaLevel && step == 'D') {
+      ++valleysCount;
+    }
+  }
+  return valleysCount;
+}
+
+/*************************************************/
+/* HERE STARTS THE PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+using namespace std;
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
