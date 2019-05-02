b03f43c8f1bab9657aa39011d0648b9ecdf33827
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-05-02 10:18:33 -0300

    Add greedy-florist challenge

---
+++ hacker-rank/greedy-florist.cpp
@@ -0,0 +1,90 @@
+/*
+ * https://www.hackerrank.com/challenges/greedy-florist/
+ */
+
+#include <bits/stdc++.h>
+
+int getMinimumCost(int friends, std::vector<int> flowers) {
+    std::sort(std::rbegin(flowers), std::rend(flowers));
+    int multiplyer = 0;
+    int cost = 0;
+    for (std::size_t flowerIndex = 0; flowerIndex < flowers.size(); ++flowerIndex) {
+        if (flowerIndex % friends == 0) {
+            ++multiplyer;
+        }
+        cost += multiplyer * flowers[flowerIndex];
+    }
+    return cost;
+}
+
+/*************************************************/
+/* HERE STARTS THE PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int minimumCost = getMinimumCost(k, c);
+
+    fout << minimumCost << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
