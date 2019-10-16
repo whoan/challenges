a09b0b2a95eabebeab6a13231119571203c7f2de
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-10-16 09:47:52 -0300

    Add linked-list challenges

---
+++ hacker-rank/linked-lists/insert-a-node-at-a-specific-position-in-a-linked-list.cpp
@@ -0,0 +1,129 @@
+/*
+ * https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list
+ */
+
+#include <bits/stdc++.h>
+
+/*************************************************/
+/* HERE STARTS SOME PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*************************************************/
+/* HERE ENDS SOME PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+// this is my code
+
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    auto* newNode = new SinglyLinkedListNode(data);
+    if (position == 0) {
+        newNode->next = head;
+        return newNode;
+    }
+    SinglyLinkedListNode* current = head;
+    for (int i(1); i < position; ++i) {
+        current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+    return head;
+}
+
+/*************************************************/
+/* HERE STARTS SOME PREDEFINED CODE IN HACKERRANK */
+/*************************************************/
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
