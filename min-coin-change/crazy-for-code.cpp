40c49868768843d89919bdc1fc3e7a3b0aa0e960
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-08 16:02:06 -0300

    Add min-coin-change example

---
+++ min-coin-change/crazy-for-code.cpp
@@ -0,0 +1,44 @@
+// https://www.crazyforcode.com/coin-problem/
+
+#include <bits/stdc++.h>
+
+//int minCoins( int a[], int N, int S ){
+template <typename Coins>
+int minCoins(const Coins& a, int N, int S) {
+    /* N: Length of the array */
+
+    int *min = (int *)malloc(sizeof(int)*(S+1));  // whoan syas: ignore this the leak please
+    int i,j;
+    for(i=0;i<=S;i++)
+        min[i]= INT_MAX; // start with extremly large value
+
+    min[0]=0;
+    for(i=1;i<=S;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            if(i>=a[j])
+            {
+                if(min[i-a[j]]+1<min[i])
+                min[i] = min[i-a[j]]+1;
+            }
+        }
+    }
+
+    if(min[S]== INT_MAX)
+        return -1;
+
+    return min[S];
+}
+
+
+// FROM HERE, STUFF NOT RELATED TO THE ALGORITHM
+
+// forward call to match function signature
+template <typename Coins>
+int minCoinsChange(const Coins& coins, int target) {
+  return minCoins(coins, coins.size(), target);
+}
+
+// see https://github.com/whoan/snip
+// snip("https://raw.githubusercontent.com/whoan/challenges/min-coin-change/min-coin-change/base.cpp")
