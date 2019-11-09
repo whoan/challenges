4dbe88395ee050cb7c078a5d056a8940f8766617
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-08 23:44:35 -0300

    Add helper function to test code with docker

---
+++ dtest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dtest() {
+  declare -a tst_volumes
+  local param
+  for param in "$@"; do
+    if [[ $param =~ ^/ ]]; then
+      tst_volumes+=(-v"$param":"$param")
+    elif [ -f "$param" ]; then
+      param=${param#~}
+      tst_volumes+=(-v"$PWD/$param":/app/"$param")
+    elif ! [[ $param =~ ^http ]]; then
+      echo "Parameters can only be urls or files" >&2
+    fi
+  done
+  docker run -ti --rm --volume=tst-cache:/root/.cache "${tst_volumes[@]}" whoan/tst:latest "$@"
+}
