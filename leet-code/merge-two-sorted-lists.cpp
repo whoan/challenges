c8a0adbb668847e2872969b0a34edc6fd46b34f4
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 20:46:21 -0300

    Add alternative solution

--- leet-code/merge-two-sorted-lists.cpp
+++ leet-code/merge-two-sorted-lists.cpp
@@ -21,3 +21,25 @@ public:
     return before;
   }
 };
+
+
+// alternative solution. Shorter but not that self-explanatory
+class Solution2 {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (!l1) {
+      return l2;
+    }
+    if (!l2) {
+      return l1;
+    }
+
+    if (l1->val <= l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    }
+
+    l2->next = mergeTwoLists(l1, l2->next);
+    return l2;
+  }
+};
