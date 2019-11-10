52e1cd17a8316726acad900a381aff18291f9a69
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 17:17:45 -0300

    Fix indentation

--- leet-code/subtree-of-another-tree.cpp
+++ leet-code/subtree-of-another-tree.cpp
@@ -9,18 +9,18 @@
  */
 class Solution {
 public:
-    bool areEqual(TreeNode* s, TreeNode* t) {
-      if (s == nullptr || t == nullptr) {
-        return s == t;  // both null
-      }
-      return s->val == t->val
-        && areEqual(s->left, t->left)
-        && areEqual(s->right, t->right);
+  bool areEqual(TreeNode* s, TreeNode* t) {
+    if (s == nullptr || t == nullptr) {
+      return s == t;  // both null
     }
+    return s->val == t->val
+      && areEqual(s->left, t->left)
+      && areEqual(s->right, t->right);
+  }
 
-    bool isSubtree(TreeNode* s, TreeNode* t) {
-        return areEqual(s, t)
-          || (s->left && isSubtree(s->left, t))
-          || (s->right && isSubtree(s->right, t));
-    }
+  bool isSubtree(TreeNode* s, TreeNode* t) {
+    return areEqual(s, t)
+      || (s->left && isSubtree(s->left, t))
+      || (s->right && isSubtree(s->right, t));
+  }
 };
