1170d63bc53573a24592f98f603a3fcf184d501d
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 00:26:38 -0300

    Update code

--- hacker-rank/binary-search-tree-lowest-common-ancestor.cpp
+++ hacker-rank/binary-search-tree-lowest-common-ancestor.cpp
@@ -10,34 +10,31 @@ const char* tst = "tst:binary-search-tree-lowest-common-ancestor";
 // see https://github.com/whoan/snip
 // snip("binary-search-tree.hpp")
 
-using snip::Node;
+using snip::TreeNode;
 
 class Solution {
-    public:
-        /* My code */
-        Node* findLca(Node* node, int min, int max) {
-            if (!node) {
-                throw std::runtime_error("There is no common ancestor");
-            }
-
-            auto data = node->data;
-            if (data >= min && data <= max) {
-                return node;
-            }
-
-            if (data >= min) {
-                return findLca(node->left, min, max);
-            }
-
-            return findLca(node->right, min, max);
+public:
+    TreeNode* findLca(TreeNode* node, int min, int max) {
+        if (!node) {
+            throw std::runtime_error("There is no common ancestor");
         }
 
-        Node *lca(Node *root, int v1, int v2) {
-            return findLca(root, std::min(v1, v2), std::max(v1, v2)) ;
+        auto data = node->data;
+        if (data >= min && data <= max) {
+            return node;
         }
-        /* End of my code */
 
-}; //End of Solution
+        if (data >= min) {
+            return findLca(node->left, min, max);
+        }
+
+        return findLca(node->right, min, max);
+    }
+
+    TreeNode *lca(TreeNode *root, int v1, int v2) {
+        return findLca(root, std::min(v1, v2), std::max(v1, v2)) ;
+    }
+};
 
 int main() {
     std::size_t numberOfElements;
@@ -47,7 +44,7 @@ int main() {
     Solution solution;   
     int v1, v2;
     std::cin >> v1 >> v2;
-    Node *ans = solution.lca(bfs.getRoot(), v1, v2);
+    TreeNode *ans = solution.lca(bfs.getRoot(), v1, v2);
     
     std::cout << ans->data;
 
