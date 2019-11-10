dedebe82471c97882ff675cc70d6394e0c4cfb20
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 00:16:35 -0300

    Use snips

--- hacker-rank/binary-search-tree-lowest-common-ancestor.cpp
+++ hacker-rank/binary-search-tree-lowest-common-ancestor.cpp
@@ -7,39 +7,13 @@
 // see https://github.com/whoan/tst
 const char* tst = "tst:binary-search-tree-lowest-common-ancestor";
 
-using namespace std;
+// see https://github.com/whoan/snip
+// snip("binary-search-tree.hpp")
 
-class Node {
-    public:
-        int data;
-        Node *left;
-        Node *right;
-        Node(int d) {
-            data = d;
-            left = NULL;
-            right = NULL;
-        }
-};
+using snip::Node;
 
 class Solution {
     public:
-      Node* insert(Node* root, int data) {
-            if(root == NULL) {
-                return new Node(data);
-            } else {
-                Node* cur;
-                if(data <= root->data) {
-                    cur = insert(root->left, data);
-                    root->left = cur;
-                } else {
-                    cur = insert(root->right, data);
-                    root->right = cur;
-               }
-
-               return root;
-           }
-        }
-
         /* My code */
         Node* findLca(Node* node, int min, int max) {
             if (!node) {
@@ -66,24 +40,14 @@ class Solution {
 }; //End of Solution
 
 int main() {
-  
-    Solution myTree;
-    Node* root = NULL;
-    
-    int t;
-    int data;
+    std::size_t numberOfElements;
+    std::cin >> numberOfElements;
+    auto bfs = snip::createBinarySearchTreeFromInput(numberOfElements);
 
-    std::cin >> t;
-
-    while(t-- > 0) {
-        std::cin >> data;
-        root = myTree.insert(root, data);
-    }
-    
+    Solution solution;   
     int v1, v2;
     std::cin >> v1 >> v2;
-  
-    Node *ans = myTree.lca(root, v1, v2);
+    Node *ans = solution.lca(bfs.getRoot(), v1, v2);
     
     std::cout << ans->data;
 
