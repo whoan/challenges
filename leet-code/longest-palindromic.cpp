2537bb8c08b9ea5c4fd6d39f7cc44071b14c3dcc
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-07 20:45:31 -0300

    Improve algorithm

--- leet-code/longest-palindromic.cpp
+++ leet-code/longest-palindromic.cpp
@@ -4,22 +4,14 @@
 
 using namespace std;
 
-auto comparator = [] (auto& first, auto& second) {
-    return first.size() < second.size();
-};
-
 class Solution {
-    std::unordered_set<std::size_t> checked;
-
     std::string longestPalindromic(const std::string& s, std::size_t middle) {
         auto left = middle, right = middle;
-        checked.insert(middle);
 
         // take all contiguous equal elements form the middle
-        std::size_t equal = 1;
         while (left > 0 and s[left-1] == s[left]) --left;
         while (right < s.size()-1 and s[right+1] == s[right]) ++right;
-        equal = std::max(right-left, equal);
+        std::size_t equal = right - left + 1;
 
         // take equal elements from the extremes
         while (left > 0 and right < s.size()-1 and s[left-1] == s[right+1]) {
@@ -28,23 +20,19 @@ class Solution {
         }
 
         // get largets palindrom
-        std::multiset<std::string, decltype(comparator)> set(comparator);
         auto current = s.substr(left, right-left+1);
-        set.insert(current);
-        if (left > equal && !checked.count(middle-equal)) {
-            set.insert(longestPalindromic(s, middle-equal));
+        std::string nextCandidate;
+        if (right < s.size() - equal) {
+            nextCandidate = longestPalindromic(s, middle+equal);
         }
-        if (right < s.size() - equal && !checked.count(middle+equal)) {
-            set.insert(longestPalindromic(s, middle+equal));
-        }
-        return *std::prev(std::end(set));
+        return current.size() > nextCandidate.size() ? current : nextCandidate;
     }
 public:
     string longestPalindrome(string s) {
         if (s.empty()) {
             return "";
         }
-        return longestPalindromic(s, s.size()/2);
+        return longestPalindromic(s, 0);
     }
 };
 
@@ -55,5 +43,6 @@ int main() {
     std::cerr << Solution().longestPalindrome("adam") << std::endl; // ada
     std::cerr << Solution().longestPalindrome("babadada") << std::endl; // adada
     std::cerr << Solution().longestPalindrome("bananas") << std::endl; // anana
+    std::cerr << Solution().longestPalindrome("aaabaaaa") << std::endl; // aaabaaa
     return 0;
 }
