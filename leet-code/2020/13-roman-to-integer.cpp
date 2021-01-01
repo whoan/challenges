e445edf937ac6713940a1c91c2058e8938df7d5d
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-01-01 16:16:15 -0300

    Add another optimization

--- leet-code/2020/13-roman-to-integer.cpp
+++ leet-code/2020/13-roman-to-integer.cpp
@@ -87,3 +87,57 @@ private:
 
     vector<int> romanToInteger('X'-'A'+1);
 };
+
+class MoreOptimizedSolution {
+public:
+    int romanToInt(string s) {
+        string_view roman = s;
+        int integer = 0;
+        for (; not roman.empty(); roman.remove_prefix(1)) {
+            if (isSubstraction(roman)) {
+                integer += romanToInteger(roman[1]) - romanToInteger(roman[0]);
+                roman.remove_prefix(1);
+            } else {
+                integer += romanToInteger(roman[0]);
+            }
+        }
+        return integer;
+    }
+
+private:
+    bool isSubstraction(string_view roman) {
+        if (roman.size() < 2) {
+            return false;
+        }
+        switch (roman[0]) {
+            case 'I':
+                return roman[1] == 'V' || roman[1] == 'X';
+            case 'X':
+                return roman[1] == 'L' || roman[1] == 'C';
+            case 'C':
+                return roman[1] == 'D' || roman[1] == 'M';
+        }
+        return false;
+    }
+
+    int romanToInteger(char roman) {
+        switch (roman) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw runtime_error("Unknown roman number");
+        }
+    }
+};
