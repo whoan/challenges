f6dd663d93f84bf8181faa68a96de609d31b33c1
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-01-23 12:33:36 -0300

    Add LC challenge #56

---
+++ leet-code/2020/0056-SLOW-merge-intervals.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/merge-intervals/
+
+class SlowSolution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(begin(intervals), end(intervals), [](auto interval1, auto interval2) {
+            return interval1[0] < interval2[0];
+        });
+        enum { START, END };
+        for (size_t i=0; i < intervals.size()-1; ++i) {
+            if (intervals[i][END] >= intervals[i+1][START]) {
+                intervals[i+1][START] = intervals[i][START];
+                intervals[i+1][END] = max(intervals[i][END], intervals[i+1][END]);
+                intervals[i].clear();
+            }
+        }
+        intervals.erase(
+            remove_if(begin(intervals), end(intervals), [] (auto& interval) { return interval.empty(); }),
+            end(intervals)
+        );
+        return move(intervals);
+    }
+};
