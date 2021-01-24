8b51bbda95570436ae014dfd93fa021acb6e4d74
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-01-24 18:18:30 -0300

    Fix silly mistake

--- leet-code/2020/0056-merge-intervals.cpp
+++ leet-code/2020/0056-merge-intervals.cpp
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/merge-intervals/
 
-class SlowSolution {
+class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(begin(intervals), end(intervals), [](auto interval1, auto interval2) {
+        sort(begin(intervals), end(intervals), [](auto& interval1, auto& interval2) {
             return interval1[0] < interval2[0];
         });
         enum { START, END };
