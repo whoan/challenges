8c92b0ad3d5dcf844331d775d0825916ccebd5e5
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-01-28 23:23:16 -0300

    Add LC challenge #94

---
+++ leet-code/2020/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        dfs(root);
+        return move(result);
+    }
+
+private:
+    void dfs(TreeNode* node) {
+        if (!node) {
+            return;
+        }
+        dfs(node->left);
+        result.push_back(node->val);
+        dfs(node->right);
+    }
+
+    vector<int> result;
+};
