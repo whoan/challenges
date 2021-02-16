77f2216b8caa04cb2c08ce27cbf6ce1740029b28
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-02-16 18:51:34 -0300

    Add LC challenge #125

---
+++ leet-code/2020/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for (int i=0, j=s.size()-1; i < j; ++i, --j) {
+            while (i < j && !isalnum(s[i])) {
+                ++i;
+            }
+            while (i < j && !isalnum(s[j])) {
+                --j;
+            }
+            if (i < j && tolower(s[i]) != tolower(s[j])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
