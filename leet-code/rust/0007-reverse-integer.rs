a689823058f1345c502b40a3e61df739a21433a1
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-06-19 12:21:31 +0200

    Add LC challenge #7 in Rust

---
+++ leet-code/rust/0007-reverse-integer.rs
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/reverse-integer/
+
+// 0ms solution
+
+impl Solution {
+    pub fn reverse(x: i32) -> i32 {
+        let mut result = 0;
+        let mut x = x;
+
+        // max allowed is == max_most_significant + max_remaining -> (-2^31 == -2147483648) or (2^31-1 == 2147483647)
+        const max_most_significant: i32 = 0x7FFFFFFF / 10; // 214748364
+        let max_remaining = match x < 0 {
+            true  => 0x7FFFFFFF % 10, // 8
+            false => 0x7FFFFFFF % 10  // 7
+        };
+
+        // reverse the x.len()-1 most significants digits
+        while x / 10 != 0 {
+            result *= 10;
+            result += i32::abs(x % 10);
+            x /= 10;
+        }
+
+        // check for overflow
+        if result > max_most_significant ||
+           (result == max_most_significant && x.abs() > max_remaining)
+        {
+            return 0;
+        }
+
+        // add the remaining to the result (no-overflow guaranteed)
+        if x < 0 {
+            result *= -1;
+        }
+        result *= 10;
+        result + x
+    }
+}
+
+/*
+Test data:
+-900000
+1534236469
+-2147483412
+-2147483648
+1563847412
+*/
