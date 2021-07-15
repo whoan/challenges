0e0718229d94f40de49eccdeca9b395745e02414
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-07-15 08:49:40 +0200

    Add alternative solutions to #205

--- leet-code/rust/0205-isomorphic-strings.rs
+++ leet-code/rust/0205-isomorphic-strings.rs
@@ -1,22 +1,76 @@
+// https://leetcode.com/problems/isomorphic-strings/
+
+/*
+Add this for safety but not needed due to constraints:
+
+    if s.len() != t.len() {
+        return false;
+    }
+    assert!(s.is_ascii());
+    assert!(t.is_ascii());
+
+*/
+
+/*
+Runtime: 0 ms, faster than 100.00% of Rust online submissions for Isomorphic Strings.
+Memory Usage: 2.1 MB, less than 100.00% of Rust online submissions for Isomorphic Strings.
+*/
 impl Solution {
     pub fn is_isomorphic(s: String, t: String) -> bool {
         let mut map = [0 as char; 128];
         let mut known = [false; 128];
 
-        let mut t: Vec<char> = t.chars().collect();
-        for (index, c) in s.chars().enumerate() {
-            let ct: char = t[index];
-            if map[ct as usize] == 0 as char {
-                if known[c as usize] {
+        for (x, y) in s.chars().zip(t.chars().map(|y| y as usize)) {
+            if map[y] == 0 as char {
+                if known[x as usize] {
                     return false;
                 }
-                known[c as usize] = true;
-                map[ct as usize] = c;
+                known[x as usize] = true;
+                map[y] = x;
             }
-            t[index] = map[ct as usize];
+            if map[y] != x {
+                return false;
+            }
         }
-        //println!("{:?}", t);
-        s.chars().collect::<Vec<char>>() == t
-        //s.chars().cpm(t.iter()) == std::cmp::Ordering::Equal;
+        true
+    }
+}
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        let mut map = [0 as char; 128];
+        let mut known = [false; 128];
+        s.chars()
+            .zip(t.chars())
+            .all(|(x, y)| {
+                let y = y as usize;
+                if map[y] == 0 as char {
+                    if known[x as usize] {
+                        return false;
+                    }
+                    known[x as usize] = true;
+                    map[y] = x;
+                }
+                map[y] == x
+            })
+    }
+}
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        let mut map = [0u8; 128];
+        let mut known = [false; 128];
+        s.bytes()
+            .zip(t.bytes().map(|y| y as usize))
+            .all(|(x, y)| {
+                if map[y] == 0u8 {
+                    if known[x as usize] {
+                        return false;
+                    }
+                    known[x as usize] = true;
+                    map[y] = x;
+                }
+                map[y] == x
+            })
     }
 }
