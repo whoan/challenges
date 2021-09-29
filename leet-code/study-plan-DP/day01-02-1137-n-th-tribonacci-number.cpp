668c120a4fd527be8fd6823581d72559977f31ae
Juan Eugenio Abadie <juaneabadie@gmail.com>
2021-09-30 00:57:55 +0200

    Add alternative solution

--- leet-code/study-plan-DP/day01-02-1137-n-th-tribonacci-number.cpp
+++ leet-code/study-plan-DP/day01-02-1137-n-th-tribonacci-number.cpp
@@ -18,3 +18,22 @@ public:
         return std::get<2>(fibResults);
     }
 };
+
+// just using a different approach for fun
+class SolutionWithDeque {
+public:
+    int tribonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        std::deque<int> deque;
+        deque.push_back(0);
+        deque.push_back(0);
+        deque.push_back(1);
+        for (int i=2; i <= n; ++i) {
+            deque.push_back(std::accumulate(std::begin(deque), std::end(deque), 0));
+            deque.pop_front();
+        }
+        return deque.back();
+    }
+};
