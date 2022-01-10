c1e0a93eca65a5c10920f53fce2cbc074c8d9753
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-10 01:47:56 +0100

    Improve code

--- leet-code/2022/0003-longest-substring-without-repeating-characters.rs
+++ leet-code/2022/0003-longest-substring-without-repeating-characters.rs
@@ -2,16 +2,16 @@
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut seen = [0; 128];
-        let mut start : i32 = 0;
+        let mut seen = [0; 127];
+        let mut start: i32 = 0;
         let mut max : i32 = 0;
-        for (idx, current_char) in s.char_indices().map(|(i, c)| (i as i32, c as usize)) {
+        for (end, current_char) in s.char_indices().map(|(i, c)| (i as i32 + 1, c as usize)) {
             if seen[current_char] > start {
                 start = seen[current_char];
             } else {
-                max = std::cmp::max(max, idx-start+1);
+                max = std::cmp::max(max, end-start);
             }
-            seen[current_char] = idx+1;
+            seen[current_char] = end;
         }
         max
     }
