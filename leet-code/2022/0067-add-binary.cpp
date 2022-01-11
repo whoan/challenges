53303a850e48fc92681dcda2219d8d573ed34c65
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-11 23:05:14 +0100

    Add LC challenge #67

---
+++ leet-code/2022/0067-add-binary.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/add-binary/
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        return a.size() < b.size() ?
+            solution(a, b) :
+            solution(b, a);
+    }
+
+    string solution(string& shorter, string& longer) const {
+        bool carryOver = false;
+        int idxShorter = shorter.size()-1;
+        int idxLonger = longer.size()-1;
+        for (; idxShorter >= 0 || idxLonger >= 0; --idxShorter, --idxLonger) {
+            int ones = carryOver;
+            ones += idxShorter >= 0 && shorter[idxShorter] == '1';
+            ones += idxLonger  >= 0 && longer[idxLonger] == '1';
+            longer[idxLonger] = (ones % 2) + '0';
+            carryOver = ones > 1;
+        }
+        return std::move(carryOver ? string(1, '1') + longer : longer);
+    }
+};
+
+/*
+Test data:
+"11"
+"1"
+"1010"
+"1011"
+*/
