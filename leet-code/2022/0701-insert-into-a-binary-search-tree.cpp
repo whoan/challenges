2f51d9f1b19985fcfb37e9e582d34a8586e74226
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-15 01:05:46 +0100

    Add LC challenge 701

---
+++ leet-code/2022/0701-insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            return new TreeNode(val);
+        }
+        if (root->val > val) {
+            root->left = insertIntoBST(root->left, val);
+        } else {
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
+
+/*
+Test data:
+[4,2,7,1,3]
+5
+[40,20,60,10,30,50,70]
+25
+[4,2,7,1,3,null,null,null,null,null,null]
+5
+*/
