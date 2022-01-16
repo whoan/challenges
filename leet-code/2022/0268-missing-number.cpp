e767299a374904d9c2230054fd11d0104ca48a47
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-16 17:16:12 +0100

    Add LC challenge 268

---
+++ leet-code/2022/0268-missing-number.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/missing-number/
+// Easy
+
+// as numbers are contiguous, XOR (also) the indices and you'll get the rsult
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for (int idx=1; idx < nums.size(); ++idx) {
+            nums[idx] ^= nums[idx-1] ^ idx;
+        }
+        return nums.back() ^ nums.size();
+    }
+};
+
+class ClearerSolution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = 0;
+        for (int idx=0; idx < nums.size(); ++idx) {
+            result ^= nums[idx];
+            result ^= idx;
+        }
+        return result ^ nums.size();
+    }
+};
