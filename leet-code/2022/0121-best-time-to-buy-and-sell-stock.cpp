6be99441f313be0c5f3f188c99cba7cbb0d9e2d4
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-17 01:29:35 +0100

    Add LC challenge 121

---
+++ leet-code/2022/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Easy
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0, buy = prices.front();
+        for (int i=1; i < prices.size(); ++i) {
+            int today_price = prices[i];
+            if (today_price < buy) {
+                buy = today_price;
+            } else {
+                max = std::max(max, today_price-buy);
+            }
+        }
+        return max;
+    }
+};
+
+// I don't  need to store the sell price. It can always be today_price
+class NotThatGreatSolution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0, buy = prices.front(), sell = prices.front();
+        for (int i=1; i < prices.size(); ++i) {
+            int today_price = prices[i];
+            if (today_price < buy) {
+                buy = sell = today_price;
+            } else {
+                sell = std::max(sell, today_price);
+            }
+            max  = std::max(max, sell-buy);
+        }
+        return max;
+    }
+};
+
+/*
+Test data:
+[7,1,5,3,6,4]
+[7,6,4,3,1]
+*/
