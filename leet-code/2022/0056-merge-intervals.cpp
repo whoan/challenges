0e789d69d349411719cb8d347b35b80df23d2588
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-01-23 18:34:14 +0100

    Add LC challenge 56

---
+++ leet-code/2022/0056-merge-intervals.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/merge-intervals/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        enum {START, END};
+        int merging_index = 0;
+        for (int i=0; i < intervals.size(); ++i) {
+            int current_end = intervals[merging_index][END];
+            if (intervals[i][START] <= current_end) {
+                intervals[merging_index][END] = std::max(current_end, intervals[i][END]);
+            } else {
+                std::swap(intervals[i], intervals[++merging_index]);
+            }
+        }
+        intervals.resize(merging_index+1);
+        return move(intervals);
+    }
+};
+
+/*
+Test data:
+[[1,3],[2,6],[8,10],[15,18]]
+[[1,4],[4,5]]
+*/
