04693a81fccce0e0abb73f2ec54ac242fd39b519
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-02-28 00:04:17 +0100

    Add LC challenge 662

---
+++ leet-code/2022/0662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        std::queue<std::pair<TreeNode*, int>> queue;
+        queue.push({root, 0});
+        int result = 1;
+        unsigned right_most = 0;
+
+        while (!queue.empty()) {
+            result = std::max(unsigned(result), right_most-queue.front().second+1);
+            int size = queue.size();
+            unsigned position = 0;
+            TreeNode* node;
+            for (int i = 0; i < size; ++i) {
+                std::tie(node, position) = queue.front();
+                position *= 2;
+                queue.pop();
+                if (node->left) {
+                    queue.push({node->left, position});
+                    right_most = position;
+                }
+                ++position;
+                if (node->right) {
+                    queue.push({node->right, position});
+                    right_most = position;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+/*
+Test data:
+[0,0,0,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null,null,0,0,null]
+*/
