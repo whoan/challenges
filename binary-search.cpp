07861c14f39bae14325277d76a1c0a002518c783
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-03-26 02:37:06 +0100

    Update template

--- binary-search.cpp
+++ binary-search.cpp
@@ -3,11 +3,11 @@
 // that's what std::lower_bound or std::upper_bound do
 
 // lower bound example -> we need to find the minimum of the max values
-int binary_search(vector<int>& nums) {
+int binary_search_template(vector<int>& nums) {
     int left = 0, right = nums.size()-1;
     while (left < right) {
         int mid = left + (right-left) / 2;
-        if (nums[mid] < nums[mid+1]) {
+        if (nums[mid] < nums[mid+1]) { // ?
             left = mid + 1;
         } else {
             right = mid;
