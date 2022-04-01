eea5c32e9f1db8c910fd03778762574ee43fc6b1
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-04-02 01:34:10 +0200

    Add LC challenge 344

---
+++ leet-code/2022/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/reverse-string/
+// Easy
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0, right = s.size()-1;
+        while (left < right) {
+            std::swap(s[left++], s[right--]);
+        }
+    }
+};
