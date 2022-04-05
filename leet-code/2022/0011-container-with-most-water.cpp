33e28124d291f96973b9dbd596df7d5932ab90a5
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-04-05 10:34:22 +0200

    Add alternative solution

--- leet-code/2022/0011-container-with-most-water.cpp
+++ leet-code/2022/0011-container-with-most-water.cpp
@@ -1,6 +1,39 @@
 // https://leetcode.com/problems/container-with-most-water/
+// Medium
 
 class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max = 0;
+        for (int i = 0, j = height.size()-1; i < j; ) {
+            int amount = std::min(height[j], height[i]) * (j-i);
+            max = std::max(amount, max);
+            if (height[i] < height[j]) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return max;
+    }
+};
+
+class BranchlessSolution {
+public:
+    int maxArea(vector<int>& height) {
+        int max = 0;
+        for (int i = 0, j = height.size()-1; i < j; ) {
+            int amount = std::min(height[j], height[i]) * (j-i);
+            max = std::max(amount, max);
+            bool index = bool(height[i] < height[j]);
+            i = std::array<int, 2>{i, i+1}[index];
+            j = std::array<int, 2>{j-1, j}[index];
+        }
+        return max;
+    }
+};
+
+class OldSolution {
 public:
     int maxArea(vector<int>& height) {
         int max = (height.size()-1) * std::min(height.front(), height.back());
@@ -15,3 +48,15 @@ public:
         return max;
     }
 };
+
+/*
+Test data:
+[1,8,6,2,5,4,8,3,7]
+[1,10,2,3,4,5,10,1]
+[1,1]
+[10,1,1]
+[1,10,10]
+[1,10,1]
+[10,1,10]
+[1,2,4,3]
+*/
