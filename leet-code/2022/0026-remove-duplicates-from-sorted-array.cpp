39dac1ac2062e65f402b97f95de6a13b90fc8ce1
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-04-10 19:52:54 +0200

    Add LC challenge 26

---
+++ leet-code/2022/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left = 0;
+        for (int right = 0; right < nums.size(); ++left) {
+            nums[left] = nums[right++];
+            while (right < nums.size() && nums[left] == nums[right]) {
+                ++right;
+            }
+        }
+        return left;
+    }
+};
