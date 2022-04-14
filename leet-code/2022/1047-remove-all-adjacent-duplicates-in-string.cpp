c2a630c2da7648aa4237e452fdc7be2c8ff4b38c
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-04-14 09:13:12 +0200

    Add LC challenge 1047

---
+++ leet-code/2022/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+// Easy
+
+// optimized without extra string
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int i = 0, j = 1;
+        for (; j < s.size(); ++j) {
+            if (i < 0 || s[i] != s[j]) {
+                ++i;
+                std::swap(s[i], s[j]);
+            } else {
+                --i;
+            }
+        }
+        s.resize(i+1);
+        return s;
+    }
+};
+
+class ONSpaceSolution {
+public:
+    string removeDuplicates(string s) {
+        string result;
+        result.reserve(s.size());
+        for (char c : s) {
+            if (!result.empty() && result.back() == c) {
+                result.pop_back();
+            } else {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+};
