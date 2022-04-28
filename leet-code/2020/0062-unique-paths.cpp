695fdef3670ecb10ced6185d0677c094905bf0d5
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-04-28 19:55:43 +0200

    Update comments

--- leet-code/2020/0062-unique-paths.cpp
+++ leet-code/2020/0062-unique-paths.cpp
@@ -1,9 +1,12 @@
 // https://leetcode.com/problems/unique-paths
 
+// this solution doe snot work anymore
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
         unsigned long long total=1;
+        // use std::min_max instead
         auto [bigger, smaller] = m > n ? make_pair(m, n) : make_pair(n, m);
         for (int i=m+n-2; i > bigger-1; --i) {
             total *= i;
@@ -12,6 +15,7 @@ public:
     }
 
 private:
+    // use tail-recursion instead
     unsigned long long factorial(int n) {
         unsigned long long total = 1;
         for (size_t i=2; i <= n; ++i) {
