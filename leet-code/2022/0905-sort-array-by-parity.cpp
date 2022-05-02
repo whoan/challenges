6dec1b3fed64a7ae8af5e6aa652f642a0389dce4
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-05-02 12:38:41 +0200

    Add LC challenge 905

---
+++ leet-code/2022/0905-sort-array-by-parity.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/sort-array-by-parity/
+// Easy
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        std::partition(nums.begin(), nums.end(), [] (int n) { return n % 2 == 0; });
+        return std::move(nums);
+    }
+};
+
+class HomeMadeSolution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        for (int i = 0, j = nums.size()-1; i < j; ) {
+            if (nums[i] % 2 == 0) {
+                ++i;
+                continue;
+            }
+            if (nums[j] % 2) {
+                --j;
+                continue;
+            }
+            std::swap(nums[i], nums[j]);
+            ++i;
+            --j;
+        }
+        return std::move(nums);
+    }
+};
