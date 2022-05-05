86a2db99d4b6e64eded4252a55714e269c10b7b3
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-05-05 09:55:55 +0200

    Add LC challenge 225

---
+++ leet-code/2022/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,69 @@
+// https://leetcode.com/problems/implement-stack-using-queues/
+// Easy
+
+class MyStack {
+public:
+    void push(int x) {
+        empty_one.push(x);
+        while (!another.empty()) {
+            empty_one.push(another.front());
+            another.pop();
+        }
+        std::swap(empty_one, another);
+    }
+
+    int pop() {
+        int value = top();
+        another.pop();
+        return value;
+    }
+
+    int top() {
+        return another.front();
+    }
+
+    bool empty() {
+        return another.empty();
+    }
+
+private:
+    std::queue<int> empty_one, another;
+};
+
+// using only one queue
+class FollowUpStack {
+public:
+    void push(int x) {
+        queue.push(x);
+        for (int i = 0; i < queue.size()-1; ++i) {
+            queue.push(queue.front());
+            queue.pop();
+        }
+    }
+
+    int pop() {
+        int value = top();
+        queue.pop();
+        return value;
+    }
+
+    int top() {
+        return queue.front();
+    }
+
+    bool empty() {
+        return queue.empty();
+    }
+
+private:
+    std::queue<int> queue;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
