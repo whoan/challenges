cfdf971d5faa4d701f5d57bf372a2d03d7a1f682
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-06-09 16:01:39 +0200

    Add LC challenge 2295

---
+++ leet-code/2022/2295-replace-elements-in-an-array.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/replace-elements-in-an-array/
+// Medium
+
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        constexpr int max = 1e6+1;
+        std::array<int, max> indices;
+        indices.fill(-1);
+        for (int i = 0; i < nums.size(); ++i) {
+            indices[nums[i]] = i;
+        }
+        for (auto& operation : operations) {
+            int index = indices[operation[0]];
+            nums[index] = operation[1];
+            indices[operation[1]] = index;
+        }
+        return nums;
+    }
+};
