54e9cf3fe9531a87b5e1b145f71fe3d79f4e7ccc
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-08-28 09:57:19 +0200

    Add alternative (still not great) solution

--- leet-code/2022/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ leet-code/2022/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,18 +1,56 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 // Medium
+
 // https://en.wikipedia.org/wiki/Lowest_common_ancestor
 
 // This one is better: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/65225/4-lines-C%2B%2BJavaPythonRuby
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+// Alternative wording: what is the subtree which contains both nodes?
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return dfs(root, p, q);
+    }
+
+private:
+    TreeNode* dfs(TreeNode* node, TreeNode* p, TreeNode* q) {
+        if (node == nullptr) {
+            return nullptr;
+        }
+        if (node == p || node == q) {
+            if (found == 1) {
+                return candidate;
+            }
+            candidate = node;
+            ++found;
+        }
+        if (!found) {
+            candidate = node;
+        }
+
+        // process left subtree
+        auto ptr = dfs(node->left, p, q);
+        if (node->left == candidate && found != 2) {
+            candidate = node;
+        }
+
+        // process right subtree
+        if (!ptr) {
+            ptr = dfs(node->right, p, q);
+        }
+        if (node->right == candidate && found != 2) {
+            candidate = node;
+        }
+
+        return ptr;
+    }
+
+    int found = 0;
+    TreeNode* candidate = nullptr;
+};
+
+
 class Solution {
     stack<TreeNode*> p_path;
     stack<TreeNode*> q_path;
@@ -47,3 +85,27 @@ public:
         return true;
     }
 };
+
+/*
+Test data:
+[1,2,3,null,4]
+4
+3
+
+        1
+      /   \
+     2     3
+      \
+       4
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
