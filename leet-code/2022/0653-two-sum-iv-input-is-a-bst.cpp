d4fbc730f0c3f1b73d322db228daae303dd88306
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-10-09 11:05:19 +0200

    Add alternative solutions

--- leet-code/2022/0653-two-sum-iv-input-is-a-bst.cpp
+++ leet-code/2022/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,7 +1,24 @@
 // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
 // Easy
 
+// there's a way to optimize the space to logN it seems
+
 class Solution {
+    static constexpr int max = 1e4*2+1;
+    std::bitset<max> seen;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        int operand = k-root->val+1e4;
+        if (seen[operand >= 0 && operand < max ? operand : max-1]) {
+            return true;
+        }
+        seen[root->val+1e4] = true;
+        return (root->left && findTarget(root->left, k))
+            || (root->right && findTarget(root->right, k));
+    }
+};
+
+class AlternativeSolution {
     static constexpr int max = 1e4*2+1;
     std::bitset<max> seen;
 public:
@@ -13,6 +30,21 @@ public:
         }
         seen[root->val+1e4] = true;
         return (root->left && findTarget(root->left, k))
+            || (root->right && findTarget(root->right, k));
+    }
+};
+
+// is it an improvement here to make it branchless? Not sure if the branch predictor can guess too much. study!
+class ShorterSolution {
+    static constexpr int max = 1e4*2+1;
+    std::bitset<max> seen;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        int operand = k-root->val+1e4;
+        bool in_range = operand >= 0 && operand < max;
+        bool operand_seen = seen[in_range*operand+!in_range*(max-1)];
+        return ((seen[root->val+1e4] = !operand_seen), operand_seen)
+            || (root->left && findTarget(root->left, k))
             || (root->right && findTarget(root->right, k));
     }
 };
