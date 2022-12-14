674b8f53db763760db027bc7402b504611d9013b
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-12-14 23:34:18 +0100

    Add LC challenge 323

---
+++ leet-code/2022/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+// Medium
+
+class UnionFind {
+    std::vector<int> edges;
+public:
+    UnionFind(int n) {
+        edges.reserve(n);
+        for (int i=0; i < n; ++i) {
+            edges.push_back(i);
+        }
+    }
+
+    void union_(int a, int b) {
+        edges[find(a)] = find(b);
+    }
+
+    int find(int n) {
+        int root = n;
+        while (edges[root] != root) {
+            root = edges[root];
+        }
+        // flat
+        while (edges[n] != n) {
+            n = edges[n];
+            edges[n] = root;
+        }
+        return root;
+    }
+
+    int count() {
+        std::unordered_set<int> components;
+        for (int i = 0; i < edges.size(); ++i) {
+            components.insert(find(i));
+        }
+        return components.size();
+    }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        UnionFind uf(n);
+        for (const auto& edge : edges) {
+            uf.union_(edge[0], edge[1]);
+        }
+        return uf.count();
+    }
+};
