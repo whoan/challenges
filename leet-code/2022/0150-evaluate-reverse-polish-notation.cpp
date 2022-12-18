7ac048f101629c2ef24012e4ff6d93aa262c8ecf
Juan Eugenio Abadie <juaneabadie@gmail.com>
2022-12-18 10:36:35 +0100

    Add alternative solutions

--- leet-code/2022/0150-evaluate-reverse-polish-notation.cpp
+++ leet-code/2022/0150-evaluate-reverse-polish-notation.cpp
@@ -1,6 +1,73 @@
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/
 // Medium
 
+template<typename T>
+T pop(std::stack<T>& stack) {
+    T n = stack.top();
+    stack.pop();
+    return n;
+}
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<long long> stack;
+        for (auto& token : tokens) {
+            if (token.size() > 1 || std::isdigit(token[0])) {
+                stack.push(std::stol(token));
+            } else {
+                switch (token[0]) {
+                    case '*':
+                        stack.top() *= pop(stack);
+                        break;
+                    case '/':
+                        stack.top() /= pop(stack);
+                        break;
+                    case '+':
+                        stack.top() += pop(stack);
+                        break;
+                    case '-':
+                        stack.top() -= pop(stack);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return stack.top();
+    }
+};
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<long long> stack;
+        for (auto& token : tokens) {
+            switch (token[0]) {
+                case '*':
+                    stack.top() *= pop(stack);
+                    break;
+                case '/':
+                    stack.top() /= pop(stack);
+                    break;
+                case '+':
+                    stack.top() += pop(stack);
+                    break;
+                case '-':
+                    if (token.size() == 1) {
+                        stack.top() -= pop(stack);
+                        break;
+                    }
+                    [[fallthrough]];
+                default:
+                    stack.push(std::stol(token));
+                    break;
+            }
+        }
+        return stack.top();
+    }
+};
+
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
