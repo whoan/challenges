a059b01e152a29dd5ef9c4d997f20ad0375fd2d1
Juan Eugenio Abadie <juaneabadie@gmail.com>
2024-05-17 19:22:51 +0200

    Add LC challenge 1325

---
+++ leet-code/2024/1325-delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/delete-leaves-with-a-given-value/
+// Medium
+
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root) {
+            return nullptr;
+        }
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        if (!root->left && !root->right && root->val == target) {
+            return nullptr;
+        }
+        return root;
+
+    }
+};
