6213c79885deb76108e74aed7b5f0e2e046ef870
Juan Eugenio Abadie <juaneabadie@gmail.com>
2025-05-11 10:13:56 +0100

    update

--- leet-code/2025/1295-find-numbers-with-even-number-of-digits.cpp
+++ leet-code/2025/1295-find-numbers-with-even-number-of-digits.cpp
@@ -8,9 +8,9 @@ public:
         auto it = nums.begin();
         int sum = 0;
         for (int n = 1; n < nums.back() * 100; n *= 100) {
-            auto next = std::ranges::lower_bound(it, nums.end(), n);
+            auto next = std::lower_bound(it, nums.end(), n);
             sum += std::distance(it, next);
-            it = std::ranges::lower_bound(next, nums.end(), n*10);
+            it = std::lower_bound(next, nums.end(), n*10);
         }
         return sum;
     }
